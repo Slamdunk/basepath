c2e3346a57dafc48c3acd260ce85d38ae6b252d2
Filippo Tessarotto <zoeslam@gmail.com>
2021-03-09 15:35:03 +0100

    Fill \Laminas\View\Helper\BasePath (#2)
    * Fill \Laminas\View\Helper\BasePath
    
    * Ignore PHP req, it works anyway

--- src/BasePathMiddlewareFactory.php
+++ src/BasePathMiddlewareFactory.php
@@ -2,9 +2,13 @@
 
 namespace LosMiddleware\BasePath;
 
-use LosMiddleware\BasePath\BasePathMiddleware;
+use Laminas\View\Helper\BasePath;
+use Laminas\View\HelperPluginManager;
 use Mezzio\Helper\UrlHelper;
+use Mezzio\Template\TemplateRendererInterface;
 use Psr\Container\ContainerInterface;
+
+use function assert;
 
 final class BasePathMiddlewareFactory
 {
@@ -14,11 +18,20 @@ final class BasePathMiddlewareFactory
         $path   = $config[BasePathMiddleware::BASE_PATH] ?? '';
 
         $urlHelper = null;
-
         if ($container->has(UrlHelper::class)) {
             $urlHelper = $container->get(UrlHelper::class);
+            assert($urlHelper instanceof UrlHelper);
+        }
+        $basePathViewHelper = null;
+        if ($container->has(TemplateRendererInterface::class)) {
+            $renderer = $container->get(TemplateRendererInterface::class);
+            assert($renderer instanceof TemplateRendererInterface);
+            $viewHelperPluginManager = $container->get(HelperPluginManager::class);
+            assert($viewHelperPluginManager instanceof HelperPluginManager);
+            $basePathViewHelper = $viewHelperPluginManager->get(BasePath::class);
+            assert($basePathViewHelper instanceof BasePath);
         }
 
-        return new BasePathMiddleware($path, $urlHelper);
+        return new BasePathMiddleware($path, $urlHelper, $basePathViewHelper);
     }
 }
